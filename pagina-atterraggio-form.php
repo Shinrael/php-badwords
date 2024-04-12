fee84e826f78f40611cc0cbae1d5fd30d0095a45
Shinrael <mircoelena89@gmail.com>
2024-04-12 15:50:44 +0100

    [add] form and landing page

--- pagina-atterraggio-form.php
+++ pagina-atterraggio-form.php
@@ -1,20 +1,23 @@
-<?php 
+<?php
 
-
-
+  $paragrafo = $_GET['paragrafo'];
+  $password = $GET['password'];
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>php-badwords</title>
+  <title>Pagina atterraggio</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 <body>
-  <a href="form.html">Vai al form</a>
+  <div class="container my-5 ">
+    <a href="form.html">Torna al form</a>
+    <h1>Paragrafo: <?php echo $paragrafo ?> </h1>
+    <h2>Password: <?php echo $password ?> </h2>
+  </div>
 </body>
 </html>
